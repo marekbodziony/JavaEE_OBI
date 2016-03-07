bb4c5de522469a668b5855cda39a1da7a2637117
Marek Bodziony <marek_bodziony@o2.pl>
2016-03-08 00:35:46 +0100

    Update RezerwujBilet.java

--- RezerwujBilet.java
+++ RezerwujBilet.java
@@ -1,10 +1,19 @@
 package OBI;
 
+
 public class RezerwujBilet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Rezerwacja
+		
+		Samolot airForceOne = new Samolot("Boeing-747",150,"01.01.1998","13.07.2013");
+		
+		Lot lotDoLondynu = new Lot("Waszyngton","Londyn","09:30, 12.03.2016","17:00, 12.03.2016", airForceOne);
+	
+		Pasazer pasazer1 = new Pasazer("Jan","Kowalski","M",35);
+
+		lotDoLondynu.wyswietlInfoLotu();
+		
 	}
-
 }
