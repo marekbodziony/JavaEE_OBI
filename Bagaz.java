7d68ea4ed525bac9669f20b2198e4d50738358a4
Marek Bodziony <marek_bodziony@o2.pl>
2016-03-08 00:46:30 +0100

    Update Bagaz.java

--- Bagaz.java
+++ Bagaz.java
@@ -1,9 +1,28 @@
 package OBI;
 
-public class Bagaz {
+
+public class Bilet {
 	
 	private Pasazer pasazer;
-	private int wagaBagazu;
+	private Lot lot;
+	private String numerMiejsca;
+	private float cenaBiletu;
+	private Bagaz bagaz;
 	
+	// konstruktor1 (pasażer leci z bagażem)
+	public Bilet(Pasazer pas, Lot l, String miejsceNr, float cena, Bagaz bag){
+		pasazer = pas;
+		lot = l;
+		numerMiejsca = miejsceNr;
+		cenaBiletu = cena;
+		bagaz = bag;
+	}
+	// konstruktor2 (pasażer leci bez bagazu)
+	public Bilet(Pasazer pas, Lot l, String miejsceNr, float cena){
+		pasazer = pas;
+		lot = l;
+		numerMiejsca = miejsceNr;
+		cenaBiletu = cena;
+	}
 
 }
