6398c6c30e6cd1b3e1cd2a3934fc28ff54dab65d
Marek Bodziony <marek_bodziony@o2.pl>
2016-03-15 02:55:15 +0100

    OK

--- Bagaz.java
+++ Bagaz.java
@@ -4,6 +4,29 @@ public class Bagaz {
 	
 	private Pasazer pasazer;
 	private Lot lot;
-	private int wagaBagazu;
+	private float wagaBagazu;
 	
+	// konstruktor
+	public Bagaz(Pasazer pasazerBag, int wagaBag){
+		
+		pasazer = pasazerBag;
+		lot = pasazerBag.getLot();
+		wagaBagazu = wagaBag;
+	}
+	
+	// getters
+	public Pasazer getPasazer(){
+		return pasazer;
+	}
+	public Lot getLot(){
+		return lot;
+	}
+	public float getWagaBagazu(){
+		return wagaBagazu;
+	}
+	
+	// setters
+	public void setWagaBagazu(float wagaBag){
+		wagaBagazu = wagaBag;
+	}
 }
