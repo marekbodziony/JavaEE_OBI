0dae58129d5962d7fee11c514a65ec2287f96cff
Marek Bodziony <marek_bodziony@o2.pl>
2016-04-08 04:10:40 +0200

    done

--- Bagaz.java
+++ Bagaz.java
@@ -2,31 +2,45 @@ package OBI;
 
 public class Bagaz {
 	
-	private Pasazer pasazer;
-	private Lot lot;
 	private float wagaBagazu;
-	
-	// konstruktor
-	public Bagaz(Pasazer pasazerBag, int wagaBag){
+	private float cenaBagazu;
+	public static final float LIMIT_WAGI_BAGAZU = 20;	// 20 kg
+	public static final float PODST_CENA_BAGAZU = 15;	// 15 euro
+	public static final float ZA_NADBAGAZ = 2; 			// 2 euro za kazdy kg nadbagazu
 		
-		pasazer = pasazerBag;
-		lot = pasazerBag.getLot();
+	// konstruktor
+	public Bagaz(){
+		cenaBagazu = PODST_CENA_BAGAZU;
+	}
+	public Bagaz(float wagaBag){
 		wagaBagazu = wagaBag;
+		cenaBagazu = obliczCeneBagazu(wagaBag);
+	}
+	
+	// wyswietl info o bagazu
+	public void wyswietlInfoBagazu(){
+		System.out.println("Bagaz : " + wagaBagazu + " kg");
+		System.out.println("Cena za bagaz: " + cenaBagazu + " \u20AC");
 	}
 	
 	// getters
-	public Pasazer getPasazer(){
-		return pasazer;
-	}
-	public Lot getLot(){
-		return lot;
-	}
-	public float getWagaBagazu(){
-		return wagaBagazu;
-	}
+	public float getWagaBagazu(){ return wagaBagazu; }
+	public float getCenaBagazu(){ return cenaBagazu;}
 	
 	// setters
-	public void setWagaBagazu(float wagaBag){
+	public void setWagaBagazu(float wagaBag){ 
 		wagaBagazu = wagaBag;
+		cenaBagazu = obliczCeneBagazu(wagaBag);
+	}
+	
+	// pomocnicza metoda do obliczenia ceny bagazu (w zaleznosci od wagi)
+	private float obliczCeneBagazu(float wagaBag){
+		float cenaBag = PODST_CENA_BAGAZU;
+		if (wagaBag > LIMIT_WAGI_BAGAZU) { 
+			cenaBag += (wagaBag - LIMIT_WAGI_BAGAZU) * ZA_NADBAGAZ; 
+			cenaBag = Math.round(cenaBag*100);
+			cenaBag /= 100;
+		} 
+		return cenaBag;
 	}
 }
